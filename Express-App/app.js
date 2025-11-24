d2b6ff2de2322b01345b0dc95b77e72a9ad5e74c
sahinkaraoglan <sahinnkaraoglann@gmail.com>
2025-11-24 15:31:15 +0300

    Current-1

--- Express-App/app.js
+++ Express-App/app.js
@@ -7,7 +7,7 @@ const path = require("path");
 app.set("view engine", "pug");
 app.set("views", "./views");
 
-const adminRoutes = require("./routes/admin");
+const admin = require("./routes/admin");
 const userRoutes = require("./routes/user");
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,11 +15,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // routes
-app.use("/admin", adminRoutes);
+app.use("/admin", admin.routes);
 app.use(userRoutes);
 
 app.use((req, res) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "Page not found" });
 });
 
 app.listen(3000, () => {
--- Express-App/routes/admin.js
+++ Express-App/routes/admin.js
@@ -1,15 +1,49 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
+
+const products = [
+  {
+    name: "samsung S8",
+    price: 3000,
+    image: "1.jpg",
+    description: "iyi telefon",
+  },
+  {
+    name: "samsung S7",
+    price: 2000,
+    image: "2.jpg",
+    description: "idare eder",
+  },
+  {
+    name: "samsung S9",
+    price: 4000,
+    image: "3.jpg",
+    description: "güzel telefon",
+  },
+  {
+    name: "IPhone 7S",
+    price: 4500,
+    image: "4.jpg",
+    description: "ço iyi telefon",
+  },
+];
 
 router.get("/add-product", (req, res, next) => {
-  res.render("add-product");
+  res.render("add-product", {
+    title: "Add a new product",
+    path: "/admin/add-product",
+  });
 });
 
 router.post("/add-product", (req, res, next) => {
-  //database kayıt
-  console.log(req.body);
+  products.push({
+    name: req.body.name,
+    price: req.body.price,
+    image: req.body.image,
+    description: req.body.description,
+  });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
