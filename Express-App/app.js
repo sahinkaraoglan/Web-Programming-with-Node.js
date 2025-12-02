a7aa5d73b1540f1a6169406dbeac3482963d3221
sahinkaraoglan <sahinnkaraoglann@gmail.com>
2025-12-02 14:28:27 +0300

    Day-1

--- Express-App/app.js
+++ Express-App/app.js
@@ -15,9 +15,23 @@ const userRoutes = require("./routes/shop");
 const errorController = require("./controllers/errors");
 const sequelize = require("./utility/database");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+const Category = require("./models/category");
+const Product = require("./models/product");
+const User = require("./models/user");
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 // routes
 app.use("/admin", adminRoutes);
@@ -36,10 +50,43 @@ connection
 
 app.use(errorController.get404Page);
 
+//ilişki türü belirleme
+//Product.hasOne(Category);
+Product.belongsTo(Category, {
+  foreignKey: {
+    allowNull: false,
+  },
+});
+Category.hasMany(Product);
+
+Product.belongsTo(User);
+User.hasMany(Product);
+
 sequelize
+  //.sync({ force: true })
   .sync()
-  .then((result) => {
-    console.log(result);
+  .then(() => {
+    User.findByPk(1)
+      .then((user) => {
+        if (!user) {
+          return User.create({
+            name: "sahinkaraoglan",
+            email: "email@gmail.com",
+          });
+        }
+        return user;
+      })
+      .then((user) => {
+        Category.count().then((count) => {
+          if (count === 0) {
+            Category.bulkCreate([
+              { name: "Telefon", description: "Telefon kategorisi" },
+              { name: "Bilgisayar", description: "Bilgisayar kategorisi" },
+              { name: "Elektronik", description: "Elektronik kategorisi" },
+            ]);
+          }
+        });
+      });
   })
   .catch((err) => {
     console.log(err);
