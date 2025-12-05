f371c6d1972da1e01c79919f0eb3845915adeb8e
sahinkaraoglan <sahinnkaraoglann@gmail.com>
2025-12-05 14:58:56 +0300

    Current
    NoSQL database mongodb works.

--- Express-App/models/category.js
+++ Express-App/models/category.js
@@ -1,16 +1,52 @@
-const Sequelize = require("sequelize");
-const sequelize = require("../utility/database");
-const Category = sequelize.define("category", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  name: Sequelize.STRING,
-  description: {
-    type: Sequelize.STRING,
-    allowNull: true,
-  },
-});
+const getDb = require("../utility/database").getdb;
+const mongodb = require("mongodb");
+
+class Category {
+  constructor(name, description, id) {
+    this.name = name;
+    this.description = description;
+    this._id = id ? new mongodb.ObjectId(id) : null;
+  }
+
+  save() {
+    let db = getDb();
+
+    if (this._id) {
+      db = db
+        .collection("categories")
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      db = db.collection("categories").insertOne(this);
+    }
+
+    return db
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  }
+
+  static findAll() {
+    const db = getDb();
+    return db
+      .collection("categories")
+      .find()
+      .toArray()
+      .then((categories) => {
+        return categories;
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static findById(categoryid) {
+    const db = getDb();
+
+    return db
+      .collection("categories")
+      .findOne({ _id: new mongodb.ObjectId(categoryid) })
+      .then((category) => {
+        return category;
+      })
+      .catch((err) => console.log(err));
+  }
+}
+
 module.exports = Category;
--- Express-App/models/product.js
+++ Express-App/models/product.js
@@ -1,24 +1,74 @@
-const Sequelize = require("sequelize");
-const sequelize = require("../utility/database");
-const Product = sequelize.define("product", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  name: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false,
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: true,
-  },
-});
+const getDb = require("../utility/database").getdb;
+const mongodb = require("mongodb");
+
+class Product {
+  constructor(name, price, description, imageUrl, id, userId) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+    this._id = id ? new mongodb.ObjectId(id) : null;
+    this.userId = userId;
+  }
+
+  save() {
+    let db = getDb();
+
+    if (this._id) {
+      db = db
+        .collection("products")
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      db = db.collection("products").insertOne(this);
+    }
+
+    return db
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static findAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find()
+      .project({ description: 0 })
+      .toArray()
+      .then((products) => {
+        return products;
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static findById(productid) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .findOne({ _id: new mongodb.ObjectId(productid) })
+      .then((product) => {
+        return product;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static deleteById(productid) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(productid) })
+      .then(() => {
+        console.log("deleted");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
+
 module.exports = Product;
